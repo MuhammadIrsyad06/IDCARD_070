8b9dd27d99e124ef751e4086afcefacbaab93e93
MuhammadIrsyad06 <muhammadirsyad764@gmail.com>
2022-11-15 00:23:33 +0700

    Pembuatan File View

--- src/main/java/idcard/b/ktp/controller.java
+++ src/main/java/idcard/b/ktp/controller.java
@@ -5,10 +5,41 @@
  */
 package idcard.b.ktp;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author lenovo
  */
+@Controller
 public class controller {
+    @RequestMapping("/getData")
+    public String getData(@RequestParam("name") String text,
+                            @RequestParam("born") @DateTimeFormat(pattern="yyyy-MM-dd") Date date,
+                            Model model, @RequestParam("image")MultipartFile file) throws IOException
+    {
+       SimpleDateFormat born=new SimpleDateFormat("EEEE,dd-MMMM-yyyy");
+       String newTanggal=born.format(date);
+       
+       String blob=Base64.encodeBase64String(file.getBytes());
+       String gambar="data:image/jpeg;base64,".concat(blob);
+       
+       model.addAttribute("nm",text);
+       model.addAttribute("brn",newTanggal);
+       model.addAttribute("img",gambar);
+       
+       return"view";
+       
+    }
     
 }
